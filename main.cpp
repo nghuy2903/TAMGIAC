cc1fb4d4c653660d08d04a3940946081f1b76de2
Nguyen Huy <nghuy29032004@gmail.com>
2023-10-30 20:46:41 +0700

    Function template

--- main.cpp
+++ main.cpp
@@ -2,28 +2,34 @@
 
 using namespace std;
 
-class DIEM{
+class Point{
     private:
-        double hoanhDo, tungDo;
-      public:
-      DIEM(double x, double y);
-        double getX();
-        double getY();
+      double Abscissa , Ordinate; // Tung do - Hoanh do
+    public:
+      Point(double x, double y);
+      double getX();
+      double getY();
+      bool isTriangle(Point a, Point b); // Kiểm tra tam giac
+      void setReflectionAcrossOx(); // Đối qua trục Ox
+      void isClassifyTriangle();// Kiểm tra tam giác gì
+      int isQuadrant(); // Kiểm tra đỉnh thuộc phần tư thứ mấy
+      double setCalculateSides(); // Tính giá trị của cạnh
+      void quickSort(); // Sắp xếp cạnh tăng dần
+      void setCalculateOther(); // Tính các giá trị khác
 };
 
-class TAMGIAC{
-    private:
-      int n; // Số lượng các điểm có trong không gian
-      DIEM* ds; // Mảng chứa n điểm
-    public:
-      TAMGIAC(int m);
-      ~TAMGIAC();
-      void findMaxPerimeterTri(); // Tìm chu vi lớn nhất và in tam giác đó ra
-      double maxPerimeterTri(DIEM a, DIEM b, DIEM c); // Tính chu vi của tam giác
+class Triangle{
+  private:
+    int n; // Số lượng các điểm có trong không gian
+    Point* ds; // Mảng chứa n điểm
+  public:
+    Triangle(int m);
+    ~Triangle();
+    void findMaxPerimeterTri(); // Tìm chu vi lớn nhất và in tam giác đó ra      double maxPerimeterTri(Point a, Point b, Point c); // Tính chu vi của tam giác
 
 };
 
 int main(){
 
-    return 0;
+  return 0;
 }
